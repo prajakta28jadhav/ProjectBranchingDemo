2fd53544f0bde9c3595a7da8d14628c334aab6e6
prajakta28jadhav <prajakta28jadhav@gmail.com>
2020-12-31 18:48:26 +0530

    dev added

---
+++ 1.MethodDemo/src/com/java/methods/Test.java
@@ -0,0 +1,47 @@
+package com.java.methods;
+
+import java.util.Scanner;
+
+public class Test {
+
+	public static void main(String[] args) {
+
+		System.out.println("added line");
+		
+		int add =  30 + 20 ;
+		System.out.println("add  "+add);
+		
+		System.out.println("added line here");
+		
+		
+		
+		NoReturnTypeWithNoParameter obj = new NoReturnTypeWithNoParameter();
+		obj.add();
+		obj.sub();
+		obj.mul();
+		obj.div();
+		
+		System.out.println("==========Hello====================");
+		NoReturnTypeWithParameter obj1 = new NoReturnTypeWithParameter();
+		obj1.add(20, 40);
+		obj1.sub(20, 40);
+		obj1.mul(20, 40);
+		obj1.div(20, 40);
+
+		System.out.println("============Helloooooooooooo=====================");
+		ReturnTypeWithParameter obj2 = new ReturnTypeWithParameter();
+		System.out.println("Addition of numbers :" + obj2.add(20, 40));
+		System.out.println("Sunstraction of numbers :" + obj2.sub(20, 40));
+		System.out.println("Multiplication of numbers :" + obj2.mul(20, 40));
+		System.out.println("Addition of numbers :" + obj2.div(20, 40));
+
+		System.out.println("*******=============================================");
+		NoReturnTypeWithNoParameter obj3 = new NoReturnTypeWithNoParameter();
+		obj3.add();
+		obj3.sub();
+		obj3.mul();
+		obj3.div();
+
+	}
+
+}
